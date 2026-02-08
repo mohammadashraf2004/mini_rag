72ba4ef776b42913cb8221dbef3456a2f4ea4c59
mohammad ashraf <mohammadashrafali2004@gmail.com>
2026-02-08 06:44:53 +0200

    add FastAPI server setup, API routes, and requirements file

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from routes.base import base_router
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+app = FastAPI()
+
+app.include_router(base_router)
---
+++ routes/base.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter , FastAPI
+import os
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+)
+
+
+
+@base_router.get("/")
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    return {"message": f"Welcome to {app_name} v{app_version}"}
